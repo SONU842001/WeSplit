0439de9446ff9198d676eee46bc2a368b7063e52
SONU842001 <54150646+SONU842001@users.noreply.github.com>
2022-06-28 15:18:16 +0530

    Completed we split app in swiftui

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cost = 0.0
+    @State private var person = 2
+    @State private var tip = 20
+    
+    var totalPerPerson :Double {
+        let peopleCount = Double(person + 2)
+        let tipSelection = Double(tip)
+        let tipValue = cost / 100 * tipSelection
+        let grandTotal = cost + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+       
+    }
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    NavigationView{
+        Form {
+            Section {
+                TextField("Amount", value: $cost, format: .currency(code: Locale.current.currencyCode ?? "INR"))
+                    .keyboardType(.decimalPad)
+                Picker("Number of People",selection: $person){
+                    ForEach(2..<100){
+                        Text("\($0) people")
+                    }
+                }
+            
+            } header: {
+                Text("Total amount")
+            }
+            .navigationTitle("WeSplit")
+            Section{
+                Picker("TipPercentage",selection: $tip){
+                    ForEach(tipPercentages,id: \.self){
+                        Text($0, format: .percent)
+                    }
+                    
+                }
+                .pickerStyle(.segmented)
+            } header:{
+                Text("How much tip do you want to leave?")
+            }
+            Section{
+                Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "INR"))
+            } header: {
+                Text("Amount per person")
+            }
+        }
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
